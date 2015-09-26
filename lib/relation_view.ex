9974f7a2d755dc3a65f0c3747be30dce5ea6925c
shibacow <shibacow@gmail.com>
2015-09-26 22:54:52 +0900

    add simirarlty checker

--- lib/relation_view.ex
+++ lib/relation_view.ex
@@ -1,4 +1,5 @@
 require Poison
+require Logger
 
 defmodule VinfoTags do
 	def start_link do
@@ -7,18 +8,17 @@ defmodule VinfoTags do
 	def loop(vmap,tagmap) do
 		receive do
 			{:put,vid,tags} ->
-        vmap = Map.put(vmap,vid,tags)
-			  tagmap = Enum.reduce tags,tagmap,fn a,acc ->
-				  t = a["tag"]
-				  Map.update(acc,t,1,fn(v)-> v+1 end)
-        end
-			  loop(vmap,tagmap)
-			{:normalize}->
+			  loop(Map.put(vmap,vid,tags),tagmap)
+			{:tagput,t} ->
+			  loop(vmap,Map.update(tagmap,t,1,fn(v)-> v+1 end))
+			{:normalize,caller}->
+			  IO.puts("normalize start")
 				sz  = hd(Enum.sort(Map.to_list(tagmap),fn(a,b) -> elem(a,1) > elem(b,1) end))
-			  sz = elem(sz,1)
+			  sz = elem(sz,1) |> :math.sqrt |> :math.sqrt
 			  IO.puts("sz=#{sz}")
-				map2 = for {k,v} <- tagmap, into: %{},do: {k,1-v*9/sz}
-				loop(vmap,map2)
+				tagmap = for {k,v} <- tagmap, into: %{},do: {k,1-:math.sqrt(:math.sqrt(v))/sz}
+				send caller,{:normalize,:ok,vmap,tagmap}
+				loop(vmap,tagmap)
 			{:to_list,caller} ->
 				send caller,{:to_list,:ok,Map.to_list(tagmap)}
 				loop(vmap,tagmap)
@@ -38,6 +38,10 @@ defmodule ReadFileGenTags do
 				vid = json["video_id"]
 				tags = json["tags"]
 				send vpid,{:put,vid,tags}
+				Enum.each(tags,fn(a) ->
+					t = a["tag"]
+					send vpid,{:tagput,t}
+				end)
 			end)
 		end)
 		fname
@@ -52,23 +56,46 @@ defmodule GenerateTags do
 	end
 	def start(_type, _args) do
     import Supervisor.Spec, warn: false
-		
     children = [
       # Define workers and child supervisors to be supervised
       # worker(VercheckEx.Worker, [arg1, arg2, arg3])
     ]
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: VercheckEx.Supervisor]
+    opts = [strategy: :one_for_one, name: GenerateTags.Supervisor]
     Supervisor.start_link(children, opts)
   end
+	defp similarityVInfo(vmap,tagmap) do
+		Enum.map(Enum.with_index(Map.to_list(vmap)),fn({a,i})->
+			avid = elem(a,0)
+			atags = for k <- elem(a,1), do: k["tag"]
+			rpp = Enum.filter_map(Map.to_list(vmap),fn(x) -> elem(x,0) != avid end,fn(b)->
+				bvid = elem(b,0)
+				btags = for c <- elem(b,1), do: c["tag"]
+				s = HashSet.intersection(Enum.into(atags,HashSet.new),Enum.into(btags,HashSet.new)) |> HashSet.to_list 
+				r = Enum.reduce(s,0,fn(t,acc) -> Map.get(tagmap,t,0) + acc end)
+			{avid,bvid,r}
+			end)
+			pp = Enum.sort(rpp,fn(a,b)-> 
+				{_,_,aa} = a 
+				{_,_,bb} = b
+				aa > bb end)
+			cpp = Enum.slice(pp,0,10)
+      if rem(i,10) == 0  do
+				Enum.each(cpp,fn(kk)->
+					{a,b,c} = kk
+					IO.puts "i=#{i} a=#{a} b=#{b} c=#{c}"
+				end)
+			end
+		end)
+	end
 	def main(args) do
 		{:ok,vpid} = VinfoTags.start_link
-		path = "../videoinfo2"
+		path = "../videoinfo3"
 		timeout=100000 #100sec
 		generateTags(path,vpid,timeout)
 		{:ok,tsk}=Task.start_link(fn -> loop() end)
-		send vpid,{:normalize}
+		send vpid,{:normalize,tsk}
 		send vpid,{:to_list,tsk}
 		send vpid,{:size,tsk}
 		loop()
@@ -78,13 +105,17 @@ defmodule GenerateTags do
 			{:to_list,:ok,results} ->  
 				Enum.each(results,fn(a)->
 					{k,v} = a
-					if v < 0.8 do
-						IO.puts("k=#{k} v=#{v}")
+					if v < 0.5 do
+						Logger.debug("k=#{k} v=#{v}")
 					end
 				end)
 				loop()
 			{:size,:ok,sz} ->
-				IO.puts("size=#{sz}")
+				Logger.debug("size=#{sz}")
+				loop()
+			{:normalize,:ok,vmap,tagmap} ->
+				Logger.debug("normlaize end")
+				similarityVInfo(vmap,tagmap)
 				loop()
 		end
 	end
